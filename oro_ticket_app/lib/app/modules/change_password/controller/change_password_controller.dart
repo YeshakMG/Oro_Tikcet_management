60b1e2e36424fc53ffdee7074e4fa4644748cf46
Isaac Mesfin <yeshakmesfin481@gmail.com>
2026-03-13 04:27:22 -0700

    Fixed API URL to use company.ota.gov.et for change password

--- oro_ticket_app/lib/app/modules/change_password/controller/change_password_controller.dart
+++ oro_ticket_app/lib/app/modules/change_password/controller/change_password_controller.dart
@@ -4,7 +4,6 @@ import 'package:get/get.dart';
 import 'package:hive/hive.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ChangePasswordController extends GetxController {
   final currentPasswordController = TextEditingController();
@@ -30,7 +29,7 @@ class ChangePasswordController extends GetxController {
   );
   static const _tokenKey = 'auth_token';
 
-  final String baseUrl = dotenv.env['API_BASE_URL'] ?? 'https://company.ota.gov.et/api';
+  final String baseUrl = 'https://company.ota.gov.et/api';
 
   /// Validate and change password
   Future<void> changePassword() async {
